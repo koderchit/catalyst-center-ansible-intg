8344b2c85c497ec732a1f89e9e725b631c9573d6
shatagopasunil <shatagopa.sunil@gmail.com>
2026-02-17 19:06:52 +0530

    Fixing sanity issue

--- plugins/modules/wireless_design_workflow_manager.py
+++ plugins/modules/wireless_design_workflow_manager.py
@@ -11226,7 +11226,7 @@ class WirelessDesign(DnacBase):
             new_design_name = attr.get("new_design_name")
             called_station_id = attr.get("called_station_id")
             if called_station_id is not None:
-              called_station_id = called_station_id.upper()
+                called_station_id = called_station_id.upper()
             self.log("Evaluating AAA Radius Attribute design: {0}".format(design_name), "DEBUG")
             self.log("Requested called_station_id: {0}".format(called_station_id), "DEBUG")
             if new_design_name:
